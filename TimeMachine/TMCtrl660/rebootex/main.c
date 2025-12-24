eda3a21a1e144963cb9eca0d8d3c944ca2d869b7
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-24 14:31:11 +0100

    update

--- TimeMachine/TMCtrl660/rebootex/main.c
+++ TimeMachine/TMCtrl660/rebootex/main.c
@@ -16,11 +16,13 @@ BootLoadExConfig bleconfig = {
     .boot_storage = MS_BOOT,
     .extra_io = {
         .psp_io = {
+            .use_fatms371 = 0,
             .tm_path = "/TM/DCARK",
             .FatMount = &MsFatMount,
             .FatOpen = &MsFatOpen,
             .FatRead = &MsFatRead,
             .FatClose = &MsFatClose,
+            .UnpackBootConfig = &UnpackBootConfigArkPSP,
         }
     }
 };
@@ -46,7 +48,7 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     findBootFunctions();
     
     // patch sceboot
-    patchBootPSP(&UnpackBootConfigArkPSP);
+    patchBootPSP();
     
     // Forward Call
     return sceBoot(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
